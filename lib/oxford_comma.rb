1411aee0aae745fac5b2933430f7430b8b939c89
Erin Stansbury <ekstansbury@gmail.com>
2020-01-06 19:16:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,6 @@ def oxford_comma(array)
     elsif array.length == 2
       array.join(" and ")
     else array.length > 2
-      array[0...-2].join(", ") << ", and #{array[-1]}"
+      array[0...-1].join(", ") << ", and #{array[-1]}"
     end
 end

9a5d0ccf5fa630fcf3921c0c06325792007522b1
Erin Stansbury <ekstansbury@gmail.com>
2020-01-06 19:16:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,20 +1,11 @@
 def oxford_comma(array)
     if array.length == 0
-      array.join
+      return nil
     elsif array.length == 1
+      return "#{array[0]}"
+    elsif array.length == 2
       array.join(" and ")
     else array.length > 2
-      array.insert(-2, " and ")
-      array.join(", ")
+      array[0...-2].join(", ") << ", and #{array[-1]}"
     end
 end
-
-
-# if array.length == 0 if counter <= array.length-2 array.join(", ")   counter += 1   array.join(" and ")
-# return array
-# if array.length == 1
-# array.join = " and "
-
-#join until
-      #new_array.push(", and" + arr)
-# split into an array
